c45a79031211aed0182eda39566ee7cb82c92658
Anna <Annkvelashvili@gmail.com>
2023-11-24 18:00:29 +0400

    add

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ burgerElement.addEventListener("click", function () {
   burgerElement.classList.toggle("activeBurger");
   navigationElement.classList.toggle("navActive");
 });
+
+let accordionContainers = document.querySelectorAll(".container-accor");
+
+//for loop
+for (let i = 0; i < accordionContainers.length; i++) {
+  const element = accordionContainers[i];
+
+  element.addEventListener("click", function () {
+    this.classList.toggle("activeNew");
+  });
+}
+//forEach
+// accordionContainers.forEach(function (e) {
+//   e.addEventListener("click", function () {
+//     this.classList.toggle("activeNew");
+//   });
+// });
+
+// for of
+// for (let item of accordionContainers) {
+//   item.addEventListener("click", function () {
+//     this.classList.toggle("activeNew");
+//   });
+// }
